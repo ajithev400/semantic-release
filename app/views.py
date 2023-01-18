19cad86b3d20fd901c7d683e94a89d59dd59cdd8
ajithev400 <ajithev220@gmail.com>
2023-01-18 12:10:19 +0530

    fix: added new functions
    BREAKING CHANGE:

---
+++ app/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+
+def index(request):
+    return HttpResponse('hello all')
+
+def home(request):
+
+    return HttpResponse("home")
+
+def register(request):
+    print("register")
+    return HttpResponse("Register")
+
+def login(request):
+    # print("login")
+    return HttpResponse("login")
